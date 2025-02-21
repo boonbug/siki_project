51792f6a5db3c4fbf04da25ec29aa86ed82a69f6
boonbug <boonbugcreation@gmail.com>
2025-02-21 15:33:57 +0530

    Add modal and migration

---
+++ app/Models/RegistryContainerDetail.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class RegistryContainerDetail extends Model
+{
+    use HasFactory;
+}
